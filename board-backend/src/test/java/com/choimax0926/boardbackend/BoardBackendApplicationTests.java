7fd368ee75712f1ede4e3feca80ecbd9bc1fba63
hosing0926 <choimax0926@gmail.com>
2023-10-16 00:39:34 +0900

    Initial Commit

---
+++ board-backend/src/test/java/com/choimax0926/boardbackend/BoardBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.choimax0926.boardbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
