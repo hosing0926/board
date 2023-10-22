fc3b4be16617b2b2b47d2d897e557f219e8855ac
hosing0926 <choimax0926@gmail.com>
2023-10-22 23:49:28 +0900

    update front : index

--- board-front/pages/index.tsx
+++ board-front/pages/index.tsx
@@ -1,113 +1,41 @@
-import { useState, useEffect } from 'react';
+import {useState, useEffect} from 'react';
 
 export default function Home() {
-  const [board, setBoard] = useState([]);
+    const [boards, setBoards] = useState([]);
 
-  useEffect(() => {
-    (async () => {
-      const {data} = await (await fetch(`http://localhost:8080/post?page=1&size=8`)).json();
-      setBoard(data);
-    })();
-  }, []);
+    useEffect(() => {
+        (async () => {
+            const {data} = await (await fetch(`http://localhost:8080/post?page=1&size=8`)).json();
+            console.log(data)
+            setBoards(data);
+        })();
+    }, []);
 
-  if (!board) {
-    return <div>Loading data...</div>;
-  }
+    if (!boards) {
+        return <div>Loading data...</div>;
+    }
 
-  return (
-      <section className="text-gray-600 body-font">
-        <div className="container px-5 py-24 mx-auto">
-          <div className="flex flex-wrap -m-4">
-            <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
-              <a className="block relative h-48 rounded overflow-hidden">
+    const list = boards.map(board => (
+        <div className="lg:w-1/4 md:w-1/2 p-4 w-full" key={board.postId}>
+            <a className="block relative h-48 rounded overflow-hidden">
                 {/*<image alt="ecommerce" className="object-cover object-center w-full h-full block"*/}
                 {/*     src="https://dummyimage.com/420x260">*/}
-              </a>
-              <div className="mt-4">
-                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-                <h2 className="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
-                <p className="mt-1">$16.00</p>
-              </div>
+            </a>
+            <div className="mt-4">
+                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">{board.title}</h3>
+                <h2 className="text-gray-900 title-font text-lg font-medium">{board.content}</h2>
+                <p className="mt-1">{board.likes ? board.likes.length : 0}</p>
             </div>
-            {/*<div className="lg:w-1/4 md:w-1/2 p-4 w-full">*/}
-            {/*  <a className="block relative h-48 rounded overflow-hidden">*/}
-            {/*    /!*<img alt="ecommerce" className="object-cover object-center w-full h-full block"*!/*/}
-            {/*    /!*     src="https://dummyimage.com/421x261">*!/*/}
-            {/*  </a>*/}
-            {/*  <div className="mt-4">*/}
-            {/*    <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>*/}
-            {/*    <h2 className="text-gray-900 title-font text-lg font-medium">Shooting Stars</h2>*/}
-            {/*    <p className="mt-1">$21.15</p>*/}
-            {/*  </div>*/}
-            {/*</div>*/}
-            {/*<div className="lg:w-1/4 md:w-1/2 p-4 w-full">*/}
-            {/*  <a className="block relative h-48 rounded overflow-hidden">*/}
-            {/*    /!*<img alt="ecommerce" className="object-cover object-center w-full h-full block"*!/*/}
-            {/*    /!*     src="https://dummyimage.com/422x262">*!/*/}
-            {/*  </a>*/}
-            {/*  <div className="mt-4">*/}
-            {/*    <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>*/}
-            {/*    <h2 className="text-gray-900 title-font text-lg font-medium">Neptune</h2>*/}
-            {/*    <p className="mt-1">$12.00</p>*/}
-            {/*  </div>*/}
-            {/*</div>*/}
-            {/*<div className="lg:w-1/4 md:w-1/2 p-4 w-full">*/}
-            {/*  <a className="block relative h-48 rounded overflow-hidden">*/}
-            {/*    /!*<img alt="ecommerce" className="object-cover object-center w-full h-full block"*!/*/}
-            {/*    /!*     src="https://dummyimage.com/423x263">*!/*/}
-            {/*  </a>*/}
-            {/*  <div className="mt-4">*/}
-            {/*    <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>*/}
-            {/*    <h2 className="text-gray-900 title-font text-lg font-medium">The 400 Blows</h2>*/}
-            {/*    <p className="mt-1">$18.40</p>*/}
-            {/*  </div>*/}
-            {/*</div>*/}
-            {/*<div className="lg:w-1/4 md:w-1/2 p-4 w-full">*/}
-            {/*  <a className="block relative h-48 rounded overflow-hidden">*/}
-            {/*    /!*<img alt="ecommerce" className="object-cover object-center w-full h-full block"*!/*/}
-            {/*    /!*     src="https://dummyimage.com/424x264">*!/*/}
-            {/*  </a>*/}
-            {/*  <div className="mt-4">*/}
-            {/*    <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>*/}
-            {/*    <h2 className="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>*/}
-            {/*    <p className="mt-1">$16.00</p>*/}
-            {/*  </div>*/}
-            {/*</div>*/}
-            {/*<div className="lg:w-1/4 md:w-1/2 p-4 w-full">*/}
-            {/*  <a className="block relative h-48 rounded overflow-hidden">*/}
-            {/*    /!*<img alt="ecommerce" className="object-cover object-center w-full h-full block"*!/*/}
-            {/*    /!*     src="https://dummyimage.com/425x265">*!/*/}
-            {/*  </a>*/}
-            {/*  <div className="mt-4">*/}
-            {/*    <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>*/}
-            {/*    <h2 className="text-gray-900 title-font text-lg font-medium">Shooting Stars</h2>*/}
-            {/*    <p className="mt-1">$21.15</p>*/}
-            {/*  </div>*/}
-            {/*</div>*/}
-            {/*<div className="lg:w-1/4 md:w-1/2 p-4 w-full">*/}
-            {/*  <a className="block relative h-48 rounded overflow-hidden">*/}
-            {/*    /!*<img alt="ecommerce" className="object-cover object-center w-full h-full block"*!/*/}
-            {/*    /!*     src="https://dummyimage.com/427x267">*!/*/}
-            {/*  </a>*/}
-            {/*  <div className="mt-4">*/}
-            {/*    <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>*/}
-            {/*    <h2 className="text-gray-900 title-font text-lg font-medium">Neptune</h2>*/}
-            {/*    <p className="mt-1">$12.00</p>*/}
-            {/*  </div>*/}
-            {/*</div>*/}
-            {/*<div className="lg:w-1/4 md:w-1/2 p-4 w-full">*/}
-            {/*  <a className="block relative h-48 rounded overflow-hidden">*/}
-            {/*    /!*<img alt="ecommerce" className="object-cover object-center w-full h-full block"*!/*/}
-            {/*    /!*     src="https://dummyimage.com/428x268">*!/*/}
-            {/*  </a>*/}
-            {/*  <div className="mt-4">*/}
-            {/*    <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>*/}
-            {/*    <h2 className="text-gray-900 title-font text-lg font-medium">The 400 Blows</h2>*/}
-            {/*    <p className="mt-1">$18.40</p>*/}
-            {/*  </div>*/}
-            {/*</div>*/}
-          </div>
         </div>
-      </section>
-  )
+    ));
+
+    return (
+        <section className="text-gray-600 body-font">
+            <div className="container px-5 py-24 mx-auto">
+                <div className="flex flex-wrap -m-4">
+                    {list}
+                </div>
+            </div>
+        </section>
+    )
 }
