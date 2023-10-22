66c4d706c21ff6736859297afc267fb7895f404e
hosing0926 <choimax0926@gmail.com>
2023-10-22 22:17:10 +0900

    add page

--- board-front/pages/_app.tsx
+++ board-front/pages/_app.tsx
@@ -1,6 +1,15 @@
 import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type {AppProps} from 'next/app'
+import Layout from '@/pages/components/Layout'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({Component, pageProps}: AppProps) {
+    return (Component && Component.name) === 'Login' ||
+    (Component && Component.name) === 'SignUp' ?
+        (
+            <Component {...pageProps} />
+        ) : (
+            <Layout>
+                <Component {...pageProps} />
+            </Layout>
+        )
 }
