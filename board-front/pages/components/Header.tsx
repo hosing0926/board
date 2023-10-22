1915e8ab3e30292f97c3727f570f50391343dbf9
hosing0926 <choimax0926@gmail.com>
2023-10-22 22:57:39 +0900

    update front : next.config 수정

--- board-front/pages/components/Header.tsx
+++ board-front/pages/components/Header.tsx
@@ -21,7 +21,7 @@ export default function Header() {
                     <span className="sr-only">Open main menu</span>
                     <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
                          viewBox="0 0 17 14">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
                               d="M1 1h15M1 7h15M1 13h15"/>
                     </svg>
                 </button>
@@ -41,8 +41,8 @@ export default function Header() {
                                     className="flex items-center justify-between w-full py-2 pl-3 pr-4  text-gray-900 rounded md:w-auto hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-600 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700">Company <svg
                                 className="w-2.5 h-2.5 ml-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
                                 fill="none" viewBox="0 0 10 6">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                      stroke-width="2" d="m1 1 4 4 4-4"/>
+                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"
+                                      strokeWidth="2" d="m1 1 4 4 4-4"/>
                             </svg><
                             /button>
                         </li>
